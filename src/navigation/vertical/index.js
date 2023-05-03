b73cfd85e0f8cab033538ce2a8ffeca655ef0d5f
Luis Mukul <luis.edmch0513@gmail.com>
2023-05-03 10:12:29 -0500

    Initial Commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,18 @@
+const navigation = () => {
+  return [
+    {
+      title: 'Home',
+      path: '/home',
+      icon: 'tabler:smart-home',
+    },
+    {
+      path: '/acl',
+      action: 'read',
+      subject: 'acl-page',
+      title: 'Access Control',
+      icon: 'tabler:shield',
+    }
+  ]
+}
+
+export default navigation
